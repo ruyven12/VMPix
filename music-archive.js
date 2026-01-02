6e0c75f2637147cd38e7a72a0402e943fc40d85e
ruyven12 <ruyven12@gmail.com>
2026-01-02 08:56:43 -0500

    Update music-archive.js

--- music-archive.js
+++ music-archive.js
@@ -67,7 +67,7 @@
   // ORANGE BOX (info strip) tuning knobs (Music only)
   // ------------------------------------------------------------
   const ORANGE_BOX_HEIGHT = '56px';
-  const ORANGE_BOX_MARGIN_TOP = '10px';
+  const ORANGE_BOX_MARGIN_TOP = '2px';
   const ORANGE_BOX_MAX_WIDTH = '96%';
 
   const ORANGE_BOX_X_OFFSET = '0px';
