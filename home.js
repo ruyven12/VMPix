38fc0a76de85243b8964c3e77f6e1aa2d3e91aab
ruyven12 <ruyven12@gmail.com>
2026-01-15 12:48:15 -0500

    Update home.js

--- home.js
+++ home.js
@@ -12,10 +12,7 @@
 (function () {
   'use strict';
 
-  const DEFAULT_COPY =
-    "Welcome to the landing site for Voodoo Media. Right now this is a placeholder for more content later but for now, please make your selection above.
-	
-	Also, this page at the moment is best viewed inside a browser only and should do good with most devices. If you are viewing this from Facebook Webview (you clicked the link in Facebook), view by browser instead.";
+  const DEFAULT_COPY = `Welcome to the landing site for Voodoo Media. Right now this is a placeholder for more content later but for now, please make your selection above.\n\nAlso, this page at the moment is best viewed inside a browser only and should do good with most devices. If you are viewing this from Facebook Webview (you clicked the link in Facebook), view by browser instead.`;
 
   let _mount = null;
 
@@ -34,6 +31,10 @@
         display: grid;
         gap: 14px;
         justify-items: center;
+      }
+
+      #homeContentRoot [data-hud-main-text]{
+        white-space: pre-line;
       }
 
       #homeContentRoot .homeQuickRow{
