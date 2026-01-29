78291d64c066124dc9893b4c9b3bab9f724c6723
ruyven12 <ruyven12@gmail.com>
2026-01-29 10:31:39 -0500

    Update about-archive.js

--- about-archive.js
+++ about-archive.js
@@ -13,11 +13,9 @@ This site is a nearly-100% automated website using coding that reads from the na
     if (!mountEl) return;
     mountEl.innerHTML = `
       <div style="max-width:760px; margin:0 auto; opacity:.9; font-size:16px; line-height:1.7; letter-spacing:.04em; text-transform:none;">
-        <strong style="font-size:18px; letter-spacing:.06em;">About</strong><br><br>
-        <div style="opacity:.75; white-space:pre-wrap;">
-          About The Site
-
-This site is a nearly-100% automated website using coding that reads from the native SmugMug system and creates it in a better customizable UI. This website was also created in assistance with ChatGPT. It is also an ever-evolving website and will need work like anything else. If it breaks, let me know.
+        <strong style="font-size:20px; letter-spacing:.06em;">About The Site</strong><br><br>
+        <div style="font-size:16px; opacity:.75; white-space:pre-wrap;">
+			This site is a nearly-100% automated website using coding that reads from the native SmugMug system and creates it in a better customizable UI. This website was also created in assistance with ChatGPT. It is also an ever-evolving website and will need work like anything else. If it breaks, let me know.
         </div>
       </div>
     `;
