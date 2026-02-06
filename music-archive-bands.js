173ea52e6cb553301592a6b292aaa8aa93c68dc2
ruyven12 <ruyven12@gmail.com>
2026-02-06 14:03:48 -0500

    Update music-archive-bands.js

--- music-archive-bands.js
+++ music-archive-bands.js
@@ -3368,8 +3368,7 @@ async function downloadZipFromServer(items, suggestedName){
         <div class="overallStatsTitle">Overall Archive Stats:</div>
         <div class="overallStatsPills">
           <div class="overallStatsPill">
-            <div class="lbl">Total Bands</div>  *  
-			<div class="val"><strong>${total}</strong></div>
+            <div class="lbl">Total Bands  *  <strong>${total}</strong></div>
             <div class="overallStatsPill good">
             <div class="lbl">Bands Fully Upgraded</div>
             <div class="val"><strong>${good}</strong>  *  ${pct(good)}</div>
