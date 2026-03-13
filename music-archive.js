ec80b0187801c977674eebd6d9cc10af8f8f5d2e
ruyven12 <ruyven12@gmail.com>
2026-03-12 20:37:58 -0400

    Update music-archive.js

--- music-archive.js
+++ music-archive.js
@@ -1296,7 +1296,7 @@ if (!document.getElementById('musicContentWipeStyles')) {
             gap:4px;
           }
           .musicStatsChipName{
-            font-size:41px;
+            font-size:14px;
             font-weight:800;
             letter-spacing:.16em;
 			text-transform: none;
