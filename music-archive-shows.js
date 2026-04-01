4b14402af69476ae7a9808050b458115a01cc058
ruyven12 <ruyven12@gmail.com>
2026-04-01 14:31:31 -0400

    Update music-archive-shows.js

--- music-archive-shows.js
+++ music-archive-shows.js
@@ -854,7 +854,8 @@
           line-height: 1.25;
       }
       .showsDetailGeoCoord{
-          margin-top: 10px;
+          margin-top: 4px;
+          margin-bottom: 10px;
           font-size: 12px;
           font-weight: 700;
           color: rgba(224,232,240,0.80);
